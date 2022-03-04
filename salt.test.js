18a9da61dfa2d58e1f14ef38ad088e15fffdcef4
nospyrin <mohammedzuhair527@gmail.com>
2022-03-04 16:39:04 +0200

    add login tests

--- salt.test.js
+++ salt.test.js
@@ -1,4 +1,4 @@
-const { users, signup} = require('./salt.js');
+const { users, signup, login } = require('./salt.js');
 
 let user = {
     email: 'mohamed.zuhair@protonmail.ch',
@@ -13,7 +13,37 @@ test('successful signup', () => {
     expect(signup(user.email, user.password)).not.toBeNull();
 });
 
+test('user count increase reflected in db', () => {
+    let userCount = users.length;
+    signup(user.email, user.password);
+
+    expect(userCount + 1).toBe(users.length);
+});
+
 test('duplicate email on signup', () => {
     signup(user.email, user.password);
     expect(signup(user.email, user.password)).toBeNull();
+});
+
+describe('login', () => {
+    beforeEach(() => {
+        signup(user.email, user.password);
+    });
+    
+    afterEach(() => {
+        users.length = 0;
+    });
+
+    test('successful login', () => {
+        expect(login(user.email, user.password)).not.toBeNull();
+    });
+
+    test('wrong email', () => {
+        expect(login('imaginary.email@unicorn.net', user.password))
+            .toBeNull();
+    });
+
+    test('wrong password', () => {
+        expect(login(user.email, 'dr0$$4p')).toBeNull();
+    });
 });
